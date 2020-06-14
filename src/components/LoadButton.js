4dadcc3c6a56a83e329ddd930fb3a81d5b1d6546
Alex Wassel <awassel18@gmail.com>
2020-06-13 22:08:08 -0700

    added remaining folders

---
+++ src/components/LoadButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button, Spinner } from 'react-bootstrap';
+
+//loading animation
+export default ({
+  isLoading,
+  text,
+  loadingText,
+  className = '',
+  disabled = false,
+  ...props
+}) =>
+  <Button
+    className={`LoadButton ${className}`}
+    disabled={disabled || isLoading}
+    {...props}
+  >
+    {isLoading && <Spinner
+      as="span"
+      animation="border"
+      size="sm"
+      role="status"
+      aria-hidden="true"
+    />}{' '}
+    {isLoading ? loadingText : text}
+  </Button>;
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+class Index extends Component {
+  state = {
+    contentCached: false,
+    updateAvailable: false,
+  };
+
+  componentDidMount() {
+    const config = {
+      onUpdate: this.handleUpdate,
+    };
+
+    serviceWorker.register(config);
+  }
+
+  render() {
+    return (
+      <Router>
+        <App updateAvailable={this.state.updateAvailable} />
+      </Router>
+    );
+  }
+
+  handleUpdate = (registration) => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+    this.setState({ updateAvailable: true});
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
